fa2bfd5f4d8bd9646ee3efccae7fac039457b9c9
Chris Banks <chris.banks@digital.cabinet-office.gov.uk>
2022-07-26 15:18:27 +0100

    Replace the duplicated shell scripts with symlinks to a single one.
    Also print a usage message when arguments are missing.

---
+++ bulk-merger.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -eu
+
+cmd=$(basename "$0")
+[[ -z "$*" ]] && echo 'usage: ./[list|merge|merge_only|review] query_string' && exit 64
+[[ -r ".env" ]] && source .env
+
+export QUERY_STRING="$*"
+bundle exec rake $cmd
